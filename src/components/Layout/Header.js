0091c3f179a06ba506969f27b8e325ff78b46756
Jhom <jhomwan238@gmail.com>
2022-05-30 23:04:50 +0700

    First Commit & Push to GitHub

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg'
+import classes from './Header.module.css'
+
+const Header = (props) => {
+  return <Fragment>
+      <header className={classes.header}>
+          <h1>React Food Order App</h1>
+          <HeaderCartButton/>
+      </header>
+      <div className={classes['main-image']}>
+          <img src={mealsImage} alt="no copyright food image from unsplash.com"/>
+      </div>
+  </Fragment>;
+};
+
+export default Header;
