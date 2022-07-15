30770865e4cb50cc7f96a17c498e12c1cd964e44
Jhom <jhomwan238@gmail.com>
2022-07-15 22:28:04 +0700

    adding modal using react portal

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
   return <Fragment>
       <header className={classes.header}>
           <h1>React Food Order App</h1>
-          <HeaderCartButton/>
+          <HeaderCartButton onClick={props.onShowCart}/>
       </header>
       <div className={classes['main-image']}>
           <img src={mealsImage} alt="no copyright food image from unsplash.com"/>
