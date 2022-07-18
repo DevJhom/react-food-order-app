325430c79ac7f609ac566b9c0b4720bb9bbfccf8
Jhom <jhomwan238@gmail.com>
2022-07-18 19:58:15 +0700

    adding a Cart Reducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
-import CartContext from './cart-context'
+import CartContext from './cart-context';
+
+const defaultCartState = {
+    items: [], 
+    totalAmount: 0
+};
+
+const cartReducer = (state, action) => {
+    if(action.type === "ADD"){
+        //'state.items' is the current state snapshot
+        // 'action.item' comes from the dispatch 
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+    }
+    return defaultCartState;
+}
 
 const CartProvider = props => {
 
-    const addItemToCartHandler = (item) => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
+    const addItemToCartHandler = (item) => {
+        dispatchCartAction({type: 'ADD', item: item});
     };
 
     const removeItemFromCartHandler = (id) => {
-
+        dispatchCartAction({type: 'REMOVE', id: id});
     }
 
+    //here is the actual context values
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler,
     }
