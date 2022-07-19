fcac77a8ae67b9c0e9a9b21a9edc0b8d9c9a151a
Jhom <jhomwan238@gmail.com>
2022-07-19 22:33:56 +0700

    add bump to header cart when adding items

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 
 import CartContext from '../../store/cart-context';
 import CartIcon from "../Cart/CartIcon";
@@ -6,14 +6,33 @@ import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
 
+  const [btnIsBumped, setBtnIsBumped] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsBumped ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if(cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsBumped(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsBumped(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+
+  },[cartCtx.items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
